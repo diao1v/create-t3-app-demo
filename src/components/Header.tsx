799aca1b88e945a108745f0d5f235f1e52f3f9d9
Yiwei Diao <tuicaodanad@gmail.com>
2023-04-27 23:12:07 +1200

    change the header text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,9 @@ export const Header = () => {
   return (
     <div className="navbar bg-primary text-primary-content">
       <div className="flex-1 pl-5 text-3xl font-bold">
-        {sessionData?.user?.name ? `Notes for ${sessionData.user.name}` : ""}
+        {sessionData?.user?.name
+          ? `Notes for ${sessionData.user.name}`
+          : "Note Taker"}
       </div>
       <div className="flex-none gap-2">
         <div className="dropdown-end dropdown">
